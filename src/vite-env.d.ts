fa32935b672e10ff6211c4c1e4befb951379e3e0
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-12-16 13:40:03 +0100

    comment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,7 +6,7 @@ interface ViteTypeOptions {
 
 interface ImportMetaEnv {
 	readonly VITE_API_BASEURL: string;
-	// more env variables...
+	// here you must declare all variables that you have in .env file as it says in the restriction on row 4
 }
 
 interface ImportMeta {
